3be4aa44a357964570822201bb50a58476e73887
Ilyes BOUCHLAGHEM <ibouchla@e1r2p20.42.fr>
2018-01-08 14:08:55 +0100

    update test

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
-#include <libft.h>
-#include <stdlib.h>
-
-void	*malloc(size_t size);
+#include <memory_management.h>
 
 int	main(void)
 {
-	malloc(42);
+	void	*n[100];
+	void	*p[100];
+	void	*x[100];
+
+	for (int i = 0; i < 100; i++)
+	{
+		n[i] = malloc(64);
+		p[i] = malloc(1024);
+		x[i] = malloc(2048);
+	}
+	for (int i = 0; i < 100; i += 2)
+	{
+		free(n[i]);
+		free(p[i]);
+		free(x[i]);
+	}
+	show_alloc_mem();
 	return (0);
 }
