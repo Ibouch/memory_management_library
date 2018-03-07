e4b6292f18e5979aa7be6bf431badd365030b61c
Ilyes BOUCHLAGHEM <ibouchla@e1r5p21.42.fr>
2018-03-07 13:28:15 +0100

    make norme

--- srcs/show_alloc_mem_ex.c
+++ srcs/show_alloc_mem_ex.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   show_alloc_mem_ex.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibouchla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/07 12:58:55 by ibouchla          #+#    #+#             */
+/*   Updated: 2018/03/07 12:59:00 by ibouchla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <memory_management.h>
 
 extern t_memory			g_data;
@@ -8,7 +20,6 @@ static void	print_digit_addr(size_t p, uint8_t base)
 	const char	*s;
 
 	s = "0123456789abcdef";
-
 	if (p > 0)
 	{
 		print_digit_addr(p / base, base);
@@ -29,12 +40,32 @@ static void	show_digit_ptr(size_t addr, uint8_t n_char)
 	print_digit_addr(addr, 16);
 }
 
+static void	area_hexdump(t_area *a, size_t addr)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < a->memory_allocated)
+	{
+		if ((i % 16) == 0)
+		{
+			ft_putchar('\n');
+			ft_putstr("\033[1;34m");
+			show_digit_ptr((size_t)addr + i, 8);
+			ft_putstr("\033[1;37m");
+			ft_putchar('\t');
+		}
+		show_digit_ptr(*((unsigned char *)(size_t)a->ptr + i), 2);
+		ft_putchar(' ');
+		++i;
+	}
+}
+
 void		show_alloc_mem_ex(void)
 {
 	uint8_t	id;
 	size_t	addr;
 	t_area	*a;
-	size_t	i;
 
 	mutex_action(INIT_MUTEX);
 	id = 0;
@@ -44,22 +75,8 @@ void		show_alloc_mem_ex(void)
 		a = g_data.areas[id];
 		while (a != NULL)
 		{
-			i = 0;
-			while (i < a->memory_allocated)
-			{
-				if ((i % 16) == 0)
-				{
-					ft_putchar('\n');
-					ft_putstr("\033[1;34m");
-					show_digit_ptr((size_t)addr + i, 8);
-					ft_putstr("\033[1;37m");
-					ft_putchar('\t');
-				}
-				show_digit_ptr(*((unsigned char *)(size_t)a->ptr + i), 2);
-				ft_putchar(' ');
-				++i;
-			}
-			addr += i;
+			area_hexdump(a, addr);
+			addr += a->memory_allocated;
 			a = a->next;
 		}
 		++id;
